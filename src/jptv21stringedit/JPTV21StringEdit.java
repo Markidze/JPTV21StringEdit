2fa55b76642236a5a10902c5dd5803600fbad75f
pupil <pupil@ivkhk.ee>
2022-09-19 15:15:04 +0300

    first commit

--- src/jptv21stringedit/JPTV21StringEdit.java
+++ src/jptv21stringedit/JPTV21StringEdit.java
@@ -5,6 +5,8 @@
  */
 package jptv21stringedit;
 
+import java.util.Scanner;
+
 /**
  *
  * @author pupil
@@ -14,7 +16,56 @@ public class JPTV21StringEdit {
     /**
      * @param args the command line arguments
      */
-
-    }
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("=====StringEdit=====");
+        System.out.println("Введите текст (латиницей): ");
+        String str = scanner.nextLine();
+        int repeat = 1;
+        do{
+            System.out.println("Выберите номер функции: ");
+            System.out.println("0.Выход из программы");
+            System.out.println("1.Количество символов в тексте");
+            System.out.println("2.Количество слов в тексте");
+            System.out.println("3.Находит указанную подстроку в тексте");
+            System.out.println("4.Заменяет указанную подстроку на другую подстроку");
+            int numberFunction = scanner.nextInt();
+            scanner.nextLine();
+        switch (numberFunction) {
+            case 0:
+                System.out.println("До свидания!");
+                repeat = 0;
+                break;
+            case 1:
+                System.out.println("Вы выбрали функцию 1|Количество символов в тексте\"");
+                char [] strChar = str.toCharArray();
+                System.out.println("Количество символов: "+strChar.length);
+                break;
+            case 2:
+                System.out.println("Вы выбрали функцию 2|Количество слов в тексте\"");
+                String[] wordArray = str.split(" ");
+                System.out.println("Количество слов: "+wordArray.length);
+                break;
+            case 3:
+                System.out.println("Вы выбрали функцию 3|Находит указанную подстроку в тексте\"");
+                System.out.println("Введите подстроку: ");
+                String subStr = scanner.nextLine();
+                int indexSubStr = str.indexOf(subStr);
+                if(indexSubStr < 0) { 
+                    System.out.println("Такой подстроки в тексте нет");
+                }else{
+                    indexSubStr++;
+                    System.out.println("Подстрока находится на "+indexSubStr+ " позиции в тексте");
+                }
+                break;
+            case 4:
+                System.out.println("Вы выбрали функцию 4|Заменяет указанную подстроку на другую подстроку");
+                
+                break;
+            default:
+                break;
+        }
+        System.out.println("==============================");
+        if(repeat == 0) break;
+    } while(true);
 }
